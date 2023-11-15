460413d372c4a30d99a68b5df1e523fbb00717e9
alexaarkhipova <alexaarkhipova@yandex.ru>
2023-11-16 00:41:12 +0300

    postgreSGL added

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
 class Question(models.Model):
     tag = models.ManyToManyField(Tag)
     title = models.CharField(max_length=100)
@@ -11,6 +12,7 @@ class Question(models.Model):
     datetime = models.DateTimeField()
     rating = models.PositiveIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Answer(models.Model):
     tag = models.ManyToManyField(Tag)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,13 +21,14 @@ class Answer(models.Model):
     datetime = models.DateTimeField()
     rating = models.PositiveIntegerField()
     correct_flag = models.BooleanField()
+
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField()
     signup_date = models.DateTimeField()
     nickname = models.CharField(max_length=100)
     rating = models.PositiveIntegerField()
